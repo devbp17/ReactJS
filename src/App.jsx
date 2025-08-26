b1cdc88b0ec77b1e12ecec2a13c3af6cc02a6160
Your Name <you@example.com>
2025-08-26 07:37:40 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home'
 import Card from './Card'
 import Project from './project';
 
 function App() {
   return(
-    <HashRouter>
+    <BrowserRouter>
      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/Home' element={<Home/>}/>
       <Route path='/Card' element={<Card/>}/>
       <Route path='/project' element={<Project/>}/>
      </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
